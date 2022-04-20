943149e672b63389c0bb2b51ec3ebec710a2eee5
can97 <can97moya@gmail.com>
2022-04-20 17:06:00 +0100

    Nav completamente integrado

---
+++ scripts/nav.js
@@ -0,0 +1,19 @@
+function equis(x) {
+  x.classList.toggle("change");
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        penDropdown.classList.remove('show');
+      }
+    }
+  }
+}
