a3d170420c9eadbbbc7d5ae921a67384feaa528d
can97 <can97moya@gmail.com>
2022-05-13 17:01:21 +0100

    Subidita de cositas guays

--- public/php/user_profile_header.php
+++ public/php/user_profile_header.php
@@ -4,6 +4,7 @@ session_start();
 
 <html>
     <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" type="text/css" href="../css/user_profile_header.css" />
     <style>
     #home, #logout{
@@ -24,7 +25,7 @@ session_start();
 	</head>
 
 <body id="customer_profile">
-
+    
     <?php
 		include 'db_connect.php';
 
@@ -43,21 +44,59 @@ session_start();
 
              <div class="welcome">
 
-             <label>Bienvenido <?php echo $_SESSION['fullname']; ?></label></div>
+             <label>Bienvenido <?php echo $_SESSION['fullname']; ?></label>
+             <div class="dropdown">
+            <button class="dropbtn" onclick="myFunction();equis(this)">
+              <div class="nav-container">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+              </div>
+            </button>  
+            <div class="dropdown-content" id="myDropdown">
+              <div class="header">
+                <h2>Banco MDA</h2>
+              </div>
+              <div class="row">
+                <div class="column">
+                    <lu class="column-header">
+                      <div class="img-container">
+                          <img class="img" src="../images/clock.png" alt="bankLogo">
+                        </div>
+                        <h3 style="display:inline-block; white-space:nowrap;">Mi día a día</h3>
+                    </lu>
+                  <a href="retiro.html">Retirar dinero</a>
+                  <a href="perfil.html">Cuenta</a>
+                  <a href="pretransferencias.html">Realizar una transferencia</a>
+                </div>
+                <div class="column">
+                    <lu class="column-header">
+                        <div class="img-container">
+                            <img class="img" src="../images/bankLogo.png" alt="bankLogo">
+                          </div>
+                          <h3 style="display:inline-block; white-space:nowrap;">Conoce tu banco</h3>
+                      </lu>
+                  <a href="../../views/divisaCalculator.html">Calculadora de divisas</a>
+                  <a href="#">Más sobre nosotros</a>
+                  <a href="terminosycondiciones.html">Términos y condiciones</a>
+                </div>
+              </div>
+            </div>
+          </div>
+            </div>
             <a class="cust_home" href="user_profile.php"><input type="button" name="home" value="Inicio" id="home"></a>
             <a class="cust_logout" href="user_logout.php"><input type="button" name="logout_btn" value="Cerrar" id="logout"></a>
-
         </div>
-
         <div class="profile_nav">
         <ul>
             <a href="user_detail.php"><li class="link1">Mi cuenta</li></a>
-            <a href="new_account.php"><li class="link2">Crear cuenta nueva</li></a>
+            <a href=""><li class="link2">Mi perfil</li></a>
             <a href="user_pass.php"><li class="link3">Cambiar contraseña</li></a>
-            <a href="pretransferencias.php"><li class="link4">Realizar transferencia</li></a>
+            <a href=""><li class="link4">Hacer transferencia</li></a>
             <a href=""><li class="link5">Historial completo</li></a>
             </ul>
         </div>
-
+          <script src="../js/nav.js"></script>
     </body>
+    
 </html>
