a826b60865f791828eba1a97084195043a4ff192
Vincent <joevincentg24@gmail.com>
2022-05-13 17:54:33 +0100

    pequeño cambio al estilo

---
+++ public/php/footer.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="../css/footer.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<footer class="footer">
+    <div class="contenedorFooter">
+        <div id="link1" class="footer-box-img">
+            <a href="https://www.youtube.com" ><img class="footer-img" src="../images/YouTube-logo.png" alt="Youtube"></a>
+        </div>
+        <div id="link2" class="footer-box-img">
+            <a href="https://www.facebook.com"><img class="footer-img" src="../images/facebook.png" alt="Facebook"></a>
+        </div>
+        <div id="link3" class="footer-box-img">
+            <a href="https://www.linkedin.com" ><img class="footer-img" src="../images/linkedinLogo.png" alt="Linkedin"></a>
+        </div>
+        <div id="link4" class="footer-box-img">
+            <a href="https://www.instagram.com" ><img class="footer-img" src="../images/ig%20logo.png" alt="Instagram"></a>
+        </div>
+    </div>
+    <div class="contenedorLinks">
+        <a href="" class="link" id="link5">PREGUNTAS FRECUENTES</a>
+        <a href="" class="link" id="link6">AVISO LEGAL</a>
+        <a href="" class="link" id="link7">POLÍTICA DE COOKIES</a>
+        <a href="" class="link" id="link8">POLÍTICA DE PRIVACIDAD</a>
+        <a href="" class="link" id="link9">CONTACTANOS</a>
+    </div>
+    <p id="copy">Copyright © 2022. Todos los derechos reservados</p>
+</footer>
+
+</html>
